6d510dc8eb9655d64f6445d674de8a4e752775a2
thatsathing <thatsathing@users.noreply.github.com>
2017-06-11 00:08:22 -0400

    Cleaned some stuff up // Fixed timer, added seconds being divided into minutes // Lines now stored in an easy-on-the eyes .json file

--- teleprompter/objectoriented.js
+++ teleprompter/objectoriented.js
@@ -11,6 +11,9 @@ var fontSize = 72;
 var timeMode = false;
 var timeStarted;
 
+var scriptst;
+var scripts;
+
 var quote = new String(prompt("(1/7) Please give a quote of the day.",""));
 var quoteAuthor = new String(prompt("(2/7) Who is the author of that quote?", ""));
 var onthisday = new String(prompt("(3/7) What happened, on this day in history?", ""));
@@ -19,13 +22,34 @@ var birthdays = new String(prompt("(5/7) Whose birthdays are today? Also include
 var band = new String(prompt("(6/7) What are the band periods for today?", ""));
 var wordoftheday = new String(prompt("(7/7) And finally, give a word of the day!", ""));
 
+function preload() {
+    scriptst = loadJSON("scripts.json");
+    scripts = [];
+}
+
 function setup() {
     createCanvas(windowWidth-4,windowHeight-4);
     noStroke();
-    setcolarr();
     textSize(fontSize);
-    generateArray();
+    for (var p = 0; p < scriptst.arr.length; p++) {
+        scripts[p] = new Objlin(scriptst.arr[p].content,scriptst.arr[p].col,p);
+    }
+    // Templates
+    for (var o = 0; o < scripts.length; o++) {
+        scripts[o].content = scripts[o].content.replace("{{dayName}}",dispDay[d.getDay()])
+        scripts[o].content = scripts[o].content.replace("{{monthName}}",dispMonth[d.getMonth()])
+        scripts[o].content = scripts[o].content.replace("{{date}}",d.getDate())
+        scripts[o].content = scripts[o].content.replace("{{year}}",d.getFullYear())
+        scripts[o].content = scripts[o].content.replace("{{band}}",band)
+        scripts[o].content = scripts[o].content.replace("{{quote}}",quoteAuthor)
+        scripts[o].content = scripts[o].content.replace("{{quoteAuthor}}",quoteAuthor)
+        scripts[o].content = scripts[o].content.replace("{{onthisday}}",onthisday)
+        scripts[o].content = scripts[o].content.replace("{{onthisdayYear}}",onthisdayYear)
+        scripts[o].content = scripts[o].content.replace("{{wordoftheday}}",wordoftheday)
+        scripts[o].content = scripts[o].content.replace("{{birthdays}}",birthdays)
+    }
 }
+
 
 function draw() {
     background(0);
@@ -54,13 +78,13 @@ function keyPressed() {
 }
 
 function layoutBG() {
-    for (var p = 0; p<arrlin.length; p++) {
-        arrlin[p].bg();
+    for (var p = 0; p<scripts.length; p++) {
+        scripts[p].bg();
     }
 }
 function layoutText() {
-    for (var p = 0; p<arrlin.length; p++) {
-        arrlin[p].txt();
+    for (var p = 0; p<scripts.length; p++) {
+        scripts[p].txt();
     }
 }
 
@@ -76,6 +100,13 @@ function renderTime() {
     } else
     if (timeMode==true) {
         textAlign(RIGHT,BOTTOM);
-        text((round(round(millis()/10)/10 - round(timeStarted/10)/10))/10,windowWidth-40,windowHeight-40);
+        var passedMin = floor((floor((floor(millis()-timeStarted))/1000))/60);
+        var passedSec = floor((floor(millis()-timeStarted)/1000)%60);
+        var passedMil = floor(millis()/100-timeStarted/100)%10
+        if (passedSec<=9) {
+            text(passedMin + ":0" + passedSec + "." + passedMil,windowWidth-40,windowHeight-40);
+        } else {
+            text(passedMin + ":" + passedSec + "." + passedMil,windowWidth-40,windowHeight-40);
+        }
     }
-}
+};
--- teleprompter/objlin.js
+++ teleprompter/objlin.js
@@ -1,95 +1,24 @@
-var arrlin = [];
-
-var Scripts = [
-  "Good Morning Tigers!", 
-  "Today is " + dispDay[d.getDay()] + ", " + dispMonth[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear(), 
-  "Today's lunch menu includes:", 
-  "And now to _____", 
-  "with the quote of the day!", 
-  "",
-  "",
-  "Thank you, _____,", 
-  "for that tasty lunch report!", 
-  "",
-  "Quote of the day:", 
-  quote, 
-  "  - " + quoteAuthor, 
-  "", 
-  "Band groups: ", 
-  band, 
-  "Student Announcements:", 
-  "(READ PAPERS)",
-  "", 
-  "And now to _____", 
-  "With today's weather!", 
-  "",
-  "",
-  "Thank you, _____,",
-  "for that lovely weather report!",
-  "",
-  "On this day in history:", 
-  "In " + onthisdayYear + ",", 
-  "  " + onthisday, 
-  "", 
-  "Happy birthday to:", 
-  birthdays, 
-  "",
-  "Please stand and face your flag for", 
-  "'The Pledge of Allegiance'",
-  "(PAUSE FOR 5 SECONDS)",
-  "'I pledge allegiance...'",
-  "",
-  "",
-  "From ____, ____, _____, and the whole",
-  "behind-the scenes crew,",
-  "this is ____ saying",
-  "Have a " + wordoftheday + " day, Lafayette!"
-]
-
-function setcolarr() {
-scCol = [
-    /* COLOR REFERENCE
- * 0 = black
- * 1 = grey
- * 2 = white (text)
- * 3 = yellow
- * 4 = green
- * 5 = blue
-*/
-  3,3,3,3,3, //Seat 1
-  0,0,
-  4,4,0,4,4,4,0,4,4,4,1,0,4,4, //Seat 2
-  0,0,
-  5,5,0,5,5,5,0,5,5,0,5,5,1,5,0,0,3,3,3,3 //Seat 3 and 1
-]
-}
-function generateArray() {
-    for (var p = 0; p < Scripts.length; p++) {
-        arrlin[p] = new Objlin(Scripts[p],scCol[p],p);
-    }
-}
-
-function Objlin(content, col, splace) {
+function Objlin(content,col,splace) {
     this.content = content;
     this.col = col;
-    this.splace = splace;
+    this.splace = splace
 }
 
 Objlin.prototype.bg = function() {
     if (this.col==5) {
-        fill(color(160,160,255,100));
+        fill(color(0,0,255,100));
     } else
     if (this.col==3) {
         fill(color(255,255,0,100));
     } else
     if (this.col==4) {
-        fill(color(160,255,160,100));
+        fill(color(0,255,0,100));
     } else
     if (this.col==0) {
-        fill(color(0,0,0,100));
+        fill(color(0,0,0));
     } else
     if (this.col==1) {
-        fill(color(160,160,160,100));
+        fill(color(160,160,160));
     } 
     rect(40,this.splace*72+loc,windowWidth,fontSize);
 }
@@ -99,5 +28,5 @@ Objlin.prototype.txt = function() {
     textAlign(LEFT,TOP);
     textFont("goody old style");
     fill(255,255,255);
-    text(str(this.content),40,this.splace*72+loc);
+    text(this.content,40,this.splace*72+loc);
 }
